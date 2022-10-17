04031c6c7422147db7ceac431b16f7fdf0faac61
Konan AKOUBIA <josephakoubia@gmail.com>
2022-10-17 16:06:40 +0200

    hello_funs.h

---
+++ hello_funs.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_HELLO
+#define HEADER_HELLO
+
+void say_hello(const char* to_who);
+void say_smthg_to_smn(const char* smthg, const char* smn);
+void happy_new_year(const char* to_who);
+
+#endif
--- hello_world.c
+++ hello_world.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include "hello_funs.h"
 
 int main(void){
-	printf("Hello World!\n");
+	say_hello("Uncle Bob");
+
+	say_smthg_to_smn("heello", "hey");
+
+	happy_new_year("hello");
+
+	return 0;
 }
