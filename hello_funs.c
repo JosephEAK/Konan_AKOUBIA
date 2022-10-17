ce6e83c78ea00bd54607f613ed74c1f25df961c5
Konan AKOUBIA <josephakoubia@gmail.com>
2022-10-17 15:59:32 +0200

    hello_world.c

--- hello_funs.c
+++ hello_funs.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "hello_funs.h"
 
+void say_smthg_to_smn (const char* smthg, const char* smn){
+	printf("%s %s!\n", smthg, smn);
+}
+
 void say_hello(const char* to_who){
-	printf("Hello %s!\n",to_who);
+	say_smthg_to_smn("hello", to_who);
+}
+
+void happy_new_year(const char* to_who){
+	say_smthg_to_smn("happy new year", to_who);
+	
 }
